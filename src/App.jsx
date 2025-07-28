3a47894f61673fcfedb98d21aaf9ef79045632e5
J SURYA KIRAN <kiransurya93@kluniversity.in>
2025-07-28 10:02:33 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S205</h1>
+      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
