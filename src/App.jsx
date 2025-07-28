4738edb7344fe3cb093c44de73a878c0bbbe563d
J SURYA KIRAN <kiransurya93@kluniversity.in>
2025-07-28 10:25:29 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
